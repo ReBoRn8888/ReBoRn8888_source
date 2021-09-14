6026ad1f41e95b43ff021a2debfd633e36608ad7
Reborn <1486013862@qq.com>
2021-09-14 18:42:04 +0800

    Update

---
+++ public/about/README.md
@@ -0,0 +1,3 @@
+# ReBoRn8888.github.io
+
+Something about myself [www.reborn8888.com](www.reborn8888.com)
---
+++ public/about/js/loadmusic.js
@@ -0,0 +1,19 @@
+	const ap = new APlayer({
+		container: document.getElementById('aplayer'),
+		autoplay: true,
+		fixed: true,
+		audio: [
+			{
+				name: '夏夜',
+				artist: '四季音色',
+				url: 'http://music.163.com/song/media/outer/url?id=410042763.mp3',
+				cover: 'http://p1.music.126.net/pk-xo-6Nu5bwrA-ylVJFUA==/17885755649212510.jpg'
+			},
+			{
+				name: 'アゲイン',
+				artist: '横山克',
+				url: 'http://music.163.com/song/media/outer/url?id=30245064.mp3',
+				cover: 'http://p1.music.126.net/GSciAiBHt0OplBApxsUC-A==/7742760882881891.jpg'
+			},
+		]
+	});
